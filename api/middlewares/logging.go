f6f9a455f23b52283c196e4b0c00c31e0be11d4a
capomanpc <capomanpc@gmail.com>
2024-08-09 21:17:12 +0900

    Update 9-3

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,11 +5,36 @@ import (
 	"net/http"
 )
 
+// レスポンス内容をログとして出力するために自分で定義したResponseWriterをハンドラ関数に渡す
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int // レスポンスのステータスコードをここに格納してログとして出力する
+}
+
+// コンストラクタ
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+// resLoggingWriter内部に含まれるhttp.ResponseWriterのWriteHeaderメソッドも使用できるが
+// ステータスコードをログとして出力するためにWriteHeaderを書き換える(オーバーライドしている)
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code                      // 引数として受け取ったcodeをcodeフィールドにコピー
+	rsw.ResponseWriter.WriteHeader(code) // 元のResponseWriterのWriteHeaderメソッドを呼び出す
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		// リクエスト情報をロギング
 		log.Println(req.RequestURI, req.Method)
 
-		next.ServeHTTP(w, req)
+		// コンストラクタを呼び出して初期化
+		rlw := NewResLoggingWriter(w)
+
+		// 元々のハンドラに自前のresLoggingWriterを渡してハンドラを実行
+		next.ServeHTTP(rlw, req)
+
+		// コピーしておいたcodeフィールドを出力
+		log.Println("res: ", rlw.code)
 	})
 }
