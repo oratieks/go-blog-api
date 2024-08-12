6e3c782c313417b46cb2e5b338c554d8814d745b
capomanpc <capomanpc@gmail.com>
2024-08-12 17:44:51 +0900

    Update 11-2

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,36 +5,32 @@ import (
 	"net/http"
 )
 
-// レスポンス内容をログとして出力するために自分で定義したResponseWriterをハンドラ関数に渡す
 type resLoggingWriter struct {
 	http.ResponseWriter
-	code int // レスポンスのステータスコードをここに格納してログとして出力する
+	code int
 }
 
-// コンストラクタ
 func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
 	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
 }
 
-// resLoggingWriter内部に含まれるhttp.ResponseWriterのWriteHeaderメソッドも使用できるが
-// ステータスコードをログとして出力するためにWriteHeaderを書き換える(オーバーライドしている)
 func (rsw *resLoggingWriter) WriteHeader(code int) {
-	rsw.code = code                      // 引数として受け取ったcodeをcodeフィールドにコピー
-	rsw.ResponseWriter.WriteHeader(code) // 元のResponseWriterのWriteHeaderメソッドを呼び出す
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
 }
 
+// newTraceID()でトレースIDを取得し、リクエスト情報をロギングするミドルウェア
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		// リクエスト情報をロギング
-		log.Println(req.RequestURI, req.Method)
+		traceID := newTraceID()
 
-		// コンストラクタを呼び出して初期化
+		// リクエスト情報をロギング
+		log.Printf("[%d]%s %s\n", traceID, req.RequestURI, req.Method)
+
 		rlw := NewResLoggingWriter(w)
 
-		// 元々のハンドラに自前のresLoggingWriterを渡してハンドラを実行
 		next.ServeHTTP(rlw, req)
 
-		// コピーしておいたcodeフィールドを出力
-		log.Println("res: ", rlw.code)
+		log.Printf("[%d]res: %d", traceID, rlw.code)
 	})
 }
